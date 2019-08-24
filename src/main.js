4fc2bdbb057a6416581e14b33fac15376c35f824
Sergey Shershnev <shv.sergey70@gmail.com>
2019-08-24 14:18:07 +0300

    added load tasks by click

--- src/main.js
+++ src/main.js
@@ -7,11 +7,32 @@ import {createTaskEditTemplate} from './components/task-edit';
 import {createTaskTemplate} from './components/task';
 import {generateTaskData} from "./data/task-data";
 import {generateFilterData} from "./data/filter-data";
+import {getRandomInt} from "./util";
 
-const TASKS_COUNT = 8;
+const TASKS_COUNT_PER_LOAD = 8;
+const TASKS_COUNT = getRandomInt(20, TASKS_COUNT_PER_LOAD);
+let renderedTasksCount = 0;
 
 const renderTemplateInComponent = (component, template) => {
   component.insertAdjacentHTML(`beforeend`, template);
+};
+
+const renderPartTasks = (tasksForRender, isRenderEditCard = false) => {
+  if (isRenderEditCard) {
+    renderTemplateInComponent(boardTasksContainer, createTaskEditTemplate(tasksForRender.shift()));
+  }
+
+  tasksForRender.forEach((task) => renderTemplateInComponent(boardTasksContainer, createTaskTemplate(task)));
+};
+
+const onLoadButtonClick = (evt) => {
+  renderPartTasks(tasks.slice(renderedTasksCount, renderedTasksCount + TASKS_COUNT_PER_LOAD));
+  renderedTasksCount += TASKS_COUNT_PER_LOAD;
+
+  if (renderedTasksCount >= TASKS_COUNT) {
+    evt.target.removeEventListener(`click`, onLoadButtonClick);
+    evt.target.remove();
+  }
 };
 
 const mainBlock = document.querySelector(`.main`);
@@ -26,7 +47,9 @@ renderTemplateInComponent(mainBlock, createBoardTemplate());
 
 const boardContainer = document.querySelector(`.board.container`);
 const boardTasksContainer = boardContainer.querySelector(`.board__tasks`);
-renderTemplateInComponent(boardTasksContainer, createTaskEditTemplate(tasks.shift()));
-tasks.forEach((task) => renderTemplateInComponent(boardTasksContainer, createTaskTemplate(task)));
+
+renderPartTasks(tasks.slice(renderedTasksCount, renderedTasksCount + TASKS_COUNT_PER_LOAD), true);
+renderedTasksCount += TASKS_COUNT_PER_LOAD;
 
 renderTemplateInComponent(boardContainer, createLoadMoreButtonTemplate());
+document.querySelector(`.load-more`).addEventListener(`click`, onLoadButtonClick);
